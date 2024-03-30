02878d42ab6bb95438f69d3d74b599c7a1d588c3
Morlinnn <morlinnn@qq.com>
2024-03-30 17:11:41 +0800

    test: Updated test

--- src/test/java/example/Config.java
+++ src/test/java/example/Config.java
@@ -1,13 +1,18 @@
 package example;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.morlinnn.interfaces.Adapter;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@Setter
+@Getter
 @ToString
-public class Config {
+public class Config implements Adapter {
     private double second;
     private Map<Character, Character> expMap;
     private List<Map<Character, Integer>> expIsoMap;
@@ -17,4 +22,16 @@ public class Config {
     private Set<String> expSet;
 
     public Config() {}
+
+    @Override
+    public boolean equals(Adapter adapter) {
+        if (!(adapter instanceof Config)) return false;
+        return ((Config) adapter).getSecond() == second
+                && expMap.equals(((Config) adapter).getExpMap())
+                && expIsoMap.equals(((Config) adapter).getExpIsoMap())
+                && expList.equals(((Config) adapter).getExpList())
+                && expSet.equals(((Config) adapter).getExpSet())
+                && expSelect == ((Config) adapter).getExpSelect()
+                && expObject.equals(adapter);
+    }
 }
--- src/test/java/example/ExpObject.java
+++ src/test/java/example/ExpObject.java
@@ -1,8 +1,20 @@
 package example;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.morlinnn.interfaces.Adapter;
 
+@Setter
+@Getter
 @ToString
-public class ExpObject {
+public class ExpObject implements Adapter {
     private String name;
+    private ExpObject expObject;
+
+    @Override
+    public boolean equals(Adapter adapter) {
+        if (!(adapter instanceof ExpObject)) return false;
+        return name.equals(((ExpObject) adapter).getName());
+    }
 }
